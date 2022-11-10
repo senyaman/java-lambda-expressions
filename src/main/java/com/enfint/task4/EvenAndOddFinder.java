501ed19baa73c3b8d63efc1531a2fe47fafb83dd
Senya- Man <masenyagift0@gmail.com>
2022-11-10 20:38:10 +0200

    init commit

--- src/main/java/com/enfint/task4/EvenAndOddFinder.java
+++ src/main/java/com/enfint/task4/EvenAndOddFinder.java
@@ -1,10 +1,12 @@
 package com.enfint.task4;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EvenAndOddFinder {
+    // TODO: task4
     Map<CharacterType, Set<String>> findFromCollection(List<Set<String>> input) {
-        // TODO: task4
+
         Map<CharacterType, Set<String>> evenOdd = new HashMap<>();
 
         Set<String> evenWordsSet = new HashSet<>();
@@ -14,24 +16,30 @@ public class EvenAndOddFinder {
             evenOdd.put(CharacterType.ODD, oddWordSet);
             evenOdd.put(CharacterType.EVEN, evenWordsSet);
         } else {
-            for (Set<String> strings : input) {
-                if(strings != null) {
-                    for (String s : strings) {
-                        if (s != null && !s.equals("")) {
-                            if (s.length() % 2 == 0) {
-                                evenWordsSet.add(s);
-                                evenOdd.put(CharacterType.EVEN, evenWordsSet);
-                            } else {
-                                oddWordSet.add(s);
-                                evenOdd.put(CharacterType.ODD, oddWordSet);
-                            }
-                        }
-                    }
-                }
-            }
+            evenWordsSet = input.stream()
+                    .filter(Objects::nonNull)
+                    .map(even -> even.stream()
+                            .filter(e -> e != null && !e.isEmpty())
+                            .filter(e -> e.length() % 2 == 0)
+                            .collect(Collectors.toList()))
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toSet());
 
+            oddWordSet = input.stream()
+                    .filter(Objects::nonNull)
+                    .map(even -> even.stream()
+                            .filter(e -> e != null && !e.isEmpty())
+                            .filter(e -> e.length() % 2 != 0)
+                            .collect(Collectors.toList()))
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toSet());
+
+            evenOdd.put(CharacterType.EVEN, evenWordsSet);
+            evenOdd.put(CharacterType.ODD, oddWordSet);
         }
-        return evenOdd;
 
+        return evenOdd;
     }
+
+
 }
